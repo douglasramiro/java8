cbdafdaa2d07e67317155c1c2e92acf06b3a0488
Douglas Ramiro <douglas@cf.coop.br>
2020-11-18 20:19:32 -0300

    ajuste de app

--- src/main/java/br/com/tecnisys/java8/Endpoint.java
+++ src/main/java/br/com/tecnisys/java8/Endpoint.java
@@ -8,6 +8,6 @@ public class Endpoint {
 
     @GetMapping
     public String versao(){
-        return System.getProperty("java.runtime.version");
+        return "Versão do Java: "+System.getProperty("java.runtime.version");
     }
 }
